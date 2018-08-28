d980b60db66b22ef218802582d19f980356b9ff6
Ullas Aithal <ullasaithalk@gmail.com>
2018-08-27 20:12:25 -0500

    Initial Commit

---
+++ webapp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app>
+    <v-content>      
+        <Header/>      
+    </v-content>
+    <v-footer :fixed="fixed" app>
+      <span>&copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  }
+}
+</script>
---
+++ webapp/src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+<v-layout align-start pa-2 justify-center row grid-list-xl>
+    <v-flex>
+        <v-card>
+            <v-card-title>
+                <h1>{{heading}}</h1>
+            </v-card-title>        
+        </v-card>
+    </v-flex>
+</v-layout> 
+</template>
+<script>
+export default {
+    name:'Header',
+    data(){
+        return{
+            heading:'Weather'
+        }
+    }
+}
+</script>
