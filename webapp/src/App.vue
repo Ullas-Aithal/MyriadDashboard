f0d09125ed8fbe6bb9ea14b7d727d0abbc6a4484
= <=>
2018-10-12 23:00:43 +0530

    trying to add background

--- webapp/src/App.vue
+++ webapp/src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  <v-app dark>
-    <v-content>      
-        <Header/>
-        <Body />      
-    </v-content>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2018</span>
-    </v-footer>
-  </v-app>
+  
+    <v-app v-bind:style="{ backgroundImage: 'url(' + 'https://www.sciencedaily.com/images/2016/11/161130141053_1_900x600.jpg' + ')' }">
+      <v-content>      
+          <Header/>
+          <Body />
+      </v-content>
+      <v-footer :fixed="fixed" app>
+        <span>&copy; 2018</span>
+      </v-footer>
+    </v-app>
+  
 </template>
 
 <script>
 import Header from './components/Header'
 import Body from './components/Body'
-import moment from 'vue-moment'
+//import moment from 'vue-moment'
 export default {
   name: 'App',
   components: {
